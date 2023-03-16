c045c5c138ed0791fdae7efb8d3098df487e80ae
heya5 <yanhe.ian@outlook.com>
2023-03-16 17:09:42 +0800

    Create run_train.sh

---
+++ run_train.sh
@@ -0,0 +1,22 @@
+torchrun --nproc_per_node=8 --master_port=30001 train.py \
+    --model_name_or_path "../PTM/opt-6.7b" \
+    --data_path ./alpaca_data.json \
+    --bf16 True \
+    --output_dir ./opt-6b7-test \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 2000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap" \
+    --fsdp_transformer_layer_cls_to_wrap 'OPTDecoderLayer' \
+    --tf32 True
+    --model_max_length 2048
